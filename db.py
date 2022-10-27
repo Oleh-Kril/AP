fde828bdb23303a692425311f76017226189ea24
DKl1 <95932656+DKl1@users.noreply.github.com>
2022-10-27 16:55:30 +0300

    change db

--- db.py
+++ db.py
@@ -19,6 +19,13 @@ class User(Base):
     email = Column('email', String(45), nullable=False, unique=True)
     phone = Column('phone', String(13), nullable=False)
 
+    def __init__(self, user_name, full_name, token, email, phone):
+        self.user_name = user_name
+        self.full_name = full_name
+        self.token = token
+        self.email = email
+        self.phone = phone
+
 
 class Language(Base):
     __tablename__ = 'language'
@@ -26,6 +33,9 @@ class Language(Base):
     language_id = Column('language_id', Integer, primary_key=True, nullable=False, unique=True,
                          autoincrement=True)
     name = Column('name', String(20), nullable=False, unique=True)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Country(Base):
@@ -35,6 +45,9 @@ class Country(Base):
                         autoincrement=True)
     name = Column('name', String(20), nullable=False, unique=True)
 
+    def __init__(self, name):
+        self.name = name
+
 
 class Genre(Base):
     __tablename__ = 'genre'
@@ -43,6 +56,9 @@ class Genre(Base):
                       autoincrement=True)
     name = Column('name', String(20), nullable=False, unique=True)
 
+    def __init__(self, name):
+        self.name = name
+
 
 class Director(Base):
     __tablename__ = 'director'
@@ -50,6 +66,9 @@ class Director(Base):
     director_id = Column('director_id', Integer, primary_key=True, nullable=False, unique=True,
                          autoincrement=True)
     name = Column('name', String(30), nullable=False, unique=True)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Admin(Base):
@@ -60,6 +79,11 @@ class Admin(Base):
     user_name = Column('user_name', String(25), nullable=False, unique=True)
     full_name = Column('first_name', String(30), nullable=False)
     token = Column('token', String(260), nullable=False)
+
+    def __init__(self, user_name, full_name, token):
+        self.user_name = user_name
+        self.full_name = full_name
+        self.token = token
 
 
 class Movie(Base):
@@ -81,6 +105,19 @@ class Movie(Base):
     genre = relationship(Genre, backref='advertisement', lazy='joined')
     director = relationship(Director, backref='advertisement', lazy='joined')
 
+    def __init__(self, movie_id, title, poster_url, description, created_year, long, age_restriction, trailer_url,
+                 id_country, id_genre, id_director):
+        self.title = title
+        self.poster_url = poster_url
+        self.description = description
+        self.created_year = created_year
+        self.long = long
+        self.age_restriction = age_restriction
+        self.trailer_url = trailer_url
+        self.id_genre = id_genre
+        self.id_director = id_director
+        self.id_country = id_country
+
 
 class ScheduledMovie(Base):
     __tablename__ = "scheduled_movie"
@@ -97,7 +134,15 @@ class ScheduledMovie(Base):
     language = relationship(Language, backref='advertisement', lazy='joined')
     movie = relationship(Movie, backref='advertisement', lazy='joined')
 
-
+    def __init__(self, date_time, price, hall, type, id_language, id_movie):
+        self.date_time = date_time
+        self.price = price
+        self.type = type
+        self.hall = hall
+        self.id_movie = id_movie
+        self.id_language = id_language
+        
+        
 class Ticket(Base):
     __tablename__ = "ticket"
 
@@ -111,3 +156,8 @@ class Ticket(Base):
     user = relationship(User, backref='advertisement', lazy='joined')
     scheduled_movie = relationship(ScheduledMovie, backref='advertisement', lazy='joined')
 
+    def __init__(self, row_n, seat_n, id_user, id_scheduled_movie):
+        self.row_n = row_n
+        self.seat_n = seat_n
+        self.id_user = id_user
+        self.id_scheduled_movie = id_scheduled_movie
